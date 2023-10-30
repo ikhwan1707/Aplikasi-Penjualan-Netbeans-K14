7fed12f3bfd78954b996784a7198341e8f50e590
hafifah0608@gmail.com <tarwini18@gmail.com>
2023-10-30 10:03:05 +0700

    membuat project baru commit

---
+++ src/aplikasi/penjualan/neatbeans/k14/AplikasiPenjualanNeatbeansK14.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aplikasi.penjualan.neatbeans.k14;
+
+/**
+ *
+ * @author SMKI Utama5
+ */
+public class AplikasiPenjualanNeatbeansK14 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
